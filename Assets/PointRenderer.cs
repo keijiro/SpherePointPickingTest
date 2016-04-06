200691e0ea51ca95042bfc06edc994376758b5d1
Keijiro Takahashi <keijiro@Ultra.jt.unity3d.com>
2016-04-06 19:20:37 +0900

    Initial commit.

---
+++ Assets/PointRenderer.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class PointRenderer : MonoBehaviour
+{
+    [SerializeField]
+    int _pointCount = 100;
+
+    [SerializeField]
+    float _pointSize = 0.1f;
+
+    [SerializeField]
+    bool _strictlyUniform;
+
+    [SerializeField]
+    Shader _shader;
+
+    Material _material;
+    Mesh _mesh;
+
+    void OnEnable()
+    {
+        var shader = Shader.Find("SpherePoints");
+
+        _material = new Material(shader);
+        _material.hideFlags = HideFlags.DontSave;
+
+        var hash = new Klak.Math.XXHash(123);
+
+        var va = new List<Vector3>();
+        var ia = new List<int>();
+
+        for (var i = 0; i < _pointCount; i++)
+        {
+            var rand1 = hash.Value01(i * 2);
+            var rand2 = hash.Value01(i * 2 + 1);
+
+            var vi = va.Count;
+
+            va.Add(new Vector3(rand1, rand2, 0));
+            va.Add(new Vector3(rand1, rand2, 1));
+            va.Add(new Vector3(rand1, rand2, 2));
+            va.Add(new Vector3(rand1, rand2, 3));
+
+            ia.Add(vi);
+            ia.Add(vi + 1);
+            ia.Add(vi + 2);
+
+            ia.Add(vi + 1);
+            ia.Add(vi + 3);
+            ia.Add(vi + 2);
+        }
+
+        _mesh = new Mesh();
+        _mesh.name = "Points";
+        _mesh.hideFlags = HideFlags.DontSave;
+        _mesh.vertices = va.ToArray();
+        _mesh.SetIndices(ia.ToArray(), MeshTopology.Triangles, 0);
+        _mesh.bounds = new Bounds(Vector3.zero, Vector3.one);
+        _mesh.UploadMeshData(true);
+    }
+
+    void OnDisable()
+    {
+        DestroyImmediate(_material);
+        DestroyImmediate(_mesh);
+    }
+
+    void Update()
+    {
+        _material.SetFloat("_PointSize", _pointSize);
+
+        if (_strictlyUniform)
+            _material.EnableKeyword("_STRICT");
+        else
+            _material.DisableKeyword("_STRICT");
+
+        Graphics.DrawMesh(
+            _mesh, transform.localToWorldMatrix,
+            _material, gameObject.layer
+        );
+    }
+}
---
+++ Assets/SpherePoints.shader
@@ -0,0 +1,72 @@
+﻿Shader "SpherePoints"
+{
+    CGINCLUDE
+
+    #pragma multi_compile _ _STRICT
+
+    #include "UnityCG.cginc"
+
+    float _PointSize;
+
+    // Utility for sin/cos
+    float2 CosSin(float theta)
+    {
+        float sn, cs;
+        sincos(theta, sn, cs);
+        return float2(cs, sn);
+    }
+
+    // Uniformaly distributed points on a unit sphere
+    // http://mathworld.wolfram.com/SpherePointPicking.html
+    // https://en.wikibooks.org/wiki/Mathematica/Uniform_Spherical_Distribution
+    float3 PointPosition(float3 coord)
+    {
+    #if _STRICT
+        float theta = coord.y * UNITY_PI * 2;
+        float phi = asin(sqrt(coord.x)) * 2;
+        float2 A = CosSin(theta);
+        float2 B = CosSin(phi);
+        return float3(B.y * A.x, B.y * A.y, B.x);
+    #else
+        float u = coord.x * 2 - 1;
+        float theta = coord.y * UNITY_PI * 2;
+        return float3(CosSin(theta) * sqrt(1 - u * u), u);
+    #endif
+    }
+
+    float3 PointOffset(float3 coord)
+    {
+        float z = coord.z;
+        float aspect = _ScreenParams.x / _ScreenParams.y;
+        float dx = fmod(z, 2);
+        float dy = floor(z / 2) * aspect;
+        return float3(dx, dy, 0) * _PointSize;
+    }
+
+    float4 vert(appdata_base v) : SV_POSITION
+    {
+        float3 coord = v.vertex.xyz;
+        float4 pos = float4(PointPosition(coord), 1);
+        pos = mul(UNITY_MATRIX_MVP, pos);
+        pos.xyz += PointOffset(coord);
+        return pos;
+    }
+
+    fixed4 frag() : SV_Target
+    {
+        return 1;
+    }
+
+    ENDCG
+    SubShader
+    {
+        Pass
+        {
+            Cull Off
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            ENDCG
+        }
+    }
+}
